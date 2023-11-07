8400a716642baf8ea1514fc15a939b0fbc254560
CCDM27 <CCDM27@CCDM27.local>
2023-11-07 13:00:15 -0700

    Practica Random User

---
+++ randomUser/randomuser.js
@@ -0,0 +1,12 @@
+async function apirandomuser(){
+    const respuesta = await fetch("https://randomuser.me/api/");
+    const datos = await respuesta.json();
+
+    let foto = document.getElementById("foto");
+    let nombre = document.getElementById("nombre");
+    let telefono = document.getElementById("telefono");
+    foto.setAttribute("src", datos.results[0].picture.large);
+    nombre.innerHTML = datos.results[0].name.first+" "+datos.results[0].name.last;
+    nombre.style.color = "red";
+    telefono.innerHTML = datos.results[0].phone;
+}
